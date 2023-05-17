1d06a3a83189794dd5e960dfe86b3dd04f02da1f
211099 <211099@ids.upchiapas.edu.mx>
2023-05-16 21:39:55 -0600

    funcion completada, solo falta iterar, validar y la parte grafica

--- mochila.py
+++ mochila.py
@@ -1,26 +1,46 @@
 from itertools import permutations
+import pandas as pd
 import random
 mochila = {}
+posicion_valor_diferencia = {}
+poblacion = []
+arreglo_de_claves = []
 
+# 
+numero_de_poblacion_iniical = 2
 
 
 # un escript que lea el exel y lo deje en este formato
+def leer_exel():
+    df = pd.read_excel('datos.xlsx')
+    df = df.iloc[:, 1:]
+    inicio = 0
+    for index, row in df.iterrows():
+        if inicio <= 3:
+            inicio = inicio + 1
+        else: 
+            mochila[index+1-4] = [row[col] for col  in range(13)]
 
 
-
-poblacion = []
-arreglo_de_claves = []
-def generar_n_individuos_permutacion():
+def generar_n_individuos_aleatorios():
     global arreglo_de_claves
+    poblacion_inicial = []
+    auxiliar_claves = []
     for clave in mochila:
         arreglo_de_claves.append(clave)
-    arreglo_permutaciones = permutations(arreglo_de_claves)
-    arreglo_permutaciones = list(arreglo_permutaciones)
-    return random.sample(arreglo_permutaciones, 7)
-
+    auxiliar_claves = arreglo_de_claves
+    
+    for i in range(numero_de_poblacion_iniical):
+        auxiliar_claves_copia = auxiliar_claves[:] # crea una copia de la lista
+        random.shuffle(auxiliar_claves_copia) # baraja la copia
+        poblacion_inicial.append(auxiliar_claves_copia) # agrega la copia barajada a la población inicial
+        poblacion.append(auxiliar_claves_copia)
+    return poblacion_inicial
+   
 def seleccion_parejas(arreglo_permutaciones):
     numero_de_parejas = 1
     parejas_aleatorias = [random.sample(arreglo_permutaciones, 2) for _ in range(numero_de_parejas)]
+    print(parejas_aleatorias)
     return parejas_aleatorias
 
 def cruza(parejas_aleatorias):
@@ -55,7 +75,7 @@ def reparar_hijos(hijos_sin_reparar):
         
 
 def mutacion(hijos_reparados):
-    posibilidad_mut_individuo = 50
+    posibilidad_mut_individuo = 99
     posibilidad_mut_gen = 25
     for hijo in hijos_reparados:
         arreglo_posiciones_que_mutan=[]
@@ -63,7 +83,10 @@ def mutacion(hijos_reparados):
             for posicion in range(len(hijo)):
                 if random.randint(0,100) <= posibilidad_mut_gen :
                     arreglo_posiciones_que_mutan.append(posicion)
-        Intercambio_de_valor(arreglo_posiciones_que_mutan, hijo)
+            Intercambio_de_valor(arreglo_posiciones_que_mutan, hijo)
+        else:
+            poblacion.append(hijo)
+   
 
 #metodo de modificar gen por intercambio de valor
 def Intercambio_de_valor(arreglo_posiciones_que_mutan,hijo):
@@ -83,31 +106,109 @@ def Intercambio_de_valor(arreglo_posiciones_que_mutan,hijo):
         hijo[posicion_random] = elemento_1
     poblacion.append(hijo)
     print(poblacion)
+  
 
 
+def sumar_valores(diccionario, lista_claves):
+    # Inicializar variables para sumar los valores
+    contador = 0
+    for individuo in lista_claves:
+        resultado = []
+        nombre_elementos = []
+        categorias = []
+        valores = [0] * 11
+        aleatorio = 30   #random.randrange(0,29)
+        aux = individuo[:aleatorio]
+        # Iterar a través de las claves en la lista
+        for clave in aux:
+            # Obtener el elemento correspondiente y agregar su nombre
+            elemento = diccionario[clave]
+            nombre_elementos.append(elemento[0])
+            categorias.append(elemento[1])
+
+            # Sumar los valores del elemento
+            for i in range(2, len(elemento)):
+                print(elemento[i])
+                valores[i-2] += elemento[i]
+
+        # Retornar el resultado como una lista
+        resultado.append(nombre_elementos)
+        resultado.append(categorias)
+        resultado.append(valores)
+        posicion_valor_diferencia[contador] = resultado
+        contador += 1
+
+def obtener_diferencia():
+    print("(0)Azucar\n(1)Proteína\n(2)Grasa\n(3)Calcio\n(4)Hierro\n(5)Vitamina A\n(6)Tiamina\n(7)Riboflavina\n(8)Niacina\n(9)Folato\n(10)Vitamina C\n")
+    print("Acontinuacion agrega los valores que quieras que tenga cada atributo")
+    valores_atributos = [3200,200,200,200,200,200,100,200,200,200,200]
+    # for i in range(11):
+    #     valores_atributos.append(int(input(str(i) + ": ")))
+  
+    for elemento in range(len(posicion_valor_diferencia)):
+        aux_valores = []
+        aux = 0
+        for atributos in posicion_valor_diferencia[elemento][2]:
+            valor = atributos - valores_atributos[aux]
+            aux_valores.append(valor)
+            aux =+ 1
+        posicion_valor_diferencia[elemento].append(aux_valores)
 
 
+def calcular_suma_distancias():
+    arreglo_suma_distancias_aux = []
+    for elemento in posicion_valor_diferencia.values():
+        suma_distancias = sum(abs(elemento) for elemento in elemento[3])
+        arreglo_suma_distancias_aux.append(suma_distancias)
 
-        
-def main():
+    for i in posicion_valor_diferencia:
+        posicion_valor_diferencia[i].append(arreglo_suma_distancias_aux[i])
+
+    return suma_distancias
+
+def ordenar_elitsta():
     global poblacion
-    arreglo_permutaciones = generar_n_individuos_permutacion()
-    print("permutaciones: ", arreglo_permutaciones,"\n")
+    print("holaaaaaaaaaaaaaaaaaa")
+    combinado = list(zip(posicion_valor_diferencia.values(), poblacion))
+    ordenado = sorted(combinado, key=lambda x: x[0][4])
+    arreglos_ordenados = [tupla[1] for tupla in ordenado]
+    print(arreglos_ordenados)
+    poblacion = arreglos_ordenados
+
+def main():
+
+    global poblacion
+    leer_exel()
     
+    arreglo_permutaciones = generar_n_individuos_aleatorios()
+  
+      ##inicia el bucle
     parejas_aleatorias = seleccion_parejas(arreglo_permutaciones)
-    print("parejas aleatorias: ", parejas_aleatorias,"\n")
+    
 
     hijos_sin_reparar = cruza(parejas_aleatorias)
-    print("hijos sin reparar: ", hijos_sin_reparar,"\n")
+    
 
     hijos_reparados = reparar_hijos(hijos_sin_reparar)
-    print("hijos reparados: ", hijos_reparados,"\n")
+ 
 
     mutacion(hijos_reparados)
 
-    poblacion.extend(parejas_aleatorias)
+    sumar_valores(mochila, poblacion)
+
+    obtener_diferencia()
+
+    calcular_suma_distancias()
+
+    ordenar_elitsta()
+    
+    ##termina el bucle
     ## despues de la mutacion
-    poblacion.extend(hijos_reparados) 
-    print("poblacion final: ", poblacion,"\n")
+
+
+
 
 main()
+
+
+
